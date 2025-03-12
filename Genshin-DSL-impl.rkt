23f4d4482515120aef8f47d7838f61fd541fdcae
Will Du <duwill1412@gmail.com>
2025-03-12 16:28:20 -0400

    runtim

--- Genshin-DSL-impl.rkt
+++ Genshin-DSL-impl.rkt
@@ -3,36 +3,59 @@
 (require syntax-spec-v3 (for-syntax syntax/parse syntax/to-string))
 (require "runtime.rkt")
 
-;; define basic stats + calculations
-
-; un-lambda the attrs (should be runtime job)
 (define-syntax parse-attribute
   (lambda (stx)
     (syntax-parse stx
-      [(_ (~datum hp) percent:number)
-       #'(make-attribute (lambda (x) (character-hp x)) percent)]
-      [(_ (~datum atk) percent:number)
-       #'(make-attribute (lambda (x) (character-atk x)) percent)]
-      [(_ (~datum def) percent:number)
-       #'(make-attribute (lambda (x) (character-def x)) percent)]
-      [(_ (~datum critr) percent:number)
-       #'(make-attribute (lambda (x) (character-critr x)) percent)]
-      [(_ (~datum critd) percent:number)
-       #'(make-attribute (lambda (x) (character-critd x)) percent)]
-      [(_ (~datum em) percent:number)
-       #'(make-attribute (lambda (x) (character-em x)) percent)])))
+      ; flat stat
+      [(_ (~datum hp) amount:number)
+       #'(make-attribute 'hp amount)]
+      [(_ (~datum atk) amount:number)
+       #'(make-attribute 'atk amount)]
+      [(_ (~datum def) amount:number)
+       #'(make-attribute 'def amount)]
+      [(_ (~datum critr) amount:number)
+       #'(make-attribute 'critr amount)]
+      [(_ (~datum critd) amount:number)
+       #'(make-attribute 'critd amount)]
+      [(_ (~datum em) amount:number)
+       #'(make-attribute 'em amount)]
+      ; stat percent
+      [(_ (~datum hp) amount:number)
+       #'(make-attribute 'hp amount)]
+      [(_ (~datum atk) amount:number)
+       #'(make-attribute 'atk amount)]
+      [(_ (~datum def) amount:number)
+       #'(make-attribute 'def amount)]
+      ; flat stat by another stat percent
+      ; how to enforce that stat is hp/atk/def/em%?
+      [(_ (~datum hp) (stat percent:number))
+       #'(make-attribute 'hp (make-percent 'stat percent))]
+      [(_ (~datum atk) (stat percent:number))
+       #'(make-attribute 'atk (make-percent 'stat percent))]
+      [(_ (~datum def) (stat percent:number))
+       #'(make-attribute 'def (make-percent 'stat percent))]
+      [(_ (~datum em) (stat percent:number))
+       #'(make-attribute 'em (make-percent 'stat percent))]
+      ; stat by stat percent (shortcut)
+      [(_ (~datum hp%) percent:number)
+       #'(make-attribute 'hp (make-percent 'hp% percent))]
+      [(_ (~datum atk%) percent:number)
+       #'(make-attribute 'atk (make-percent 'atk% percent))]
+      [(_ (~datum def%) percent:number)
+       #'(make-attribute 'def (make-percent 'def% percent))]
+      )))
 
 (define-syntax define-weapon
   (lambda (stx)
     (syntax-parse stx
-      [(_ name:id damage:number (attr percent) buffs ...)
-       #'(define name (make-weapon damage (parse-attribute attr percent) (list (parse-buff buffs) ...)))])))
+      [(_ name:id atk:number (attr modifier) buffs ...)
+       #'(define name (make-weapon atk (parse-attribute attr modifier) (list (parse-buff buffs) ...)))])))
 
 (define-syntax define-skill
   (lambda (stx)
     (syntax-parse stx
-      [(_ name:id cd:number duration:number buffs ...)
-       #'(define name (make-skill cd duration (list (parse-buff buffs) ...)))])))
+      [(_ name:id cd:number #:attr (attr modifier) #:duration duration:number #:type type buffs ...)
+       #'(define name (make-skill cd (parse-attribute attr modifier) duration 'type (list (parse-buff buffs) ...)))])))
 
 (define-syntax parse-buff
   (lambda (stx)
@@ -49,15 +72,15 @@
                                         #:party-wide party-wide:boolean]))
        #'(make-unconditional-buff (parse-attribute attr percent) party-wide)]
       [(_ ((~datum applied-buff) [name:id
-                                    #:effect (attr percent)
-                                    #:limit limit:number
-                                    #:party-wide party-wide:boolean
-                                    #:duration duration]))
+                                  #:effect (attr percent)
+                                  #:limit limit:number
+                                  #:party-wide party-wide:boolean
+                                  #:duration duration]))
        #'(make-applied-buff (parse-attribute attr percent) limit party-wide duration)]
-      [(_ ((~datum damage) [(attr percent)
-                            #:duration duration:number
-                            #:type elem-type]))
-       #'(make-damage (parse-attribute attr percent) duration (parse-element elem-type))])))
+      #;[(_ ((~datum damage) [(attr percent)
+                              #:duration duration:number
+                              #:type elem-type]))
+         #'(make-damage (parse-attribute attr percent) duration (parse-element elem-type))])))
 
 (define-syntax parse-element
   (lambda (stx)
@@ -82,12 +105,12 @@
 (define-syntax define-attack-sequence
   (lambda (stx)
     (syntax-parse stx
-      [(_ name:id ([(attr percent:number) duration:number] ...
-                   #:charged  [(attr2 percent2:number) duration2:number]
-                   #:plunging [(attr3 percent3:number) duration3:number]))
-       #'(define name (make-attack-sequence (list (make-attack (parse-attribute attr percent) duration) ...)
-                                            (make-attack (parse-attribute attr2 percent2) duration2)
-                                            (make-attack (parse-attribute attr3 percent3) duration3)))])))
+      [(_ name:id ([(attr percent:number) duration:number type] ...
+                   #:charged  [(attr2 percent2:number) duration2:number type2]
+                   #:plunging [(attr3 percent3:number) duration3:number type3]))
+       #'(define name (make-attack-sequence (list (make-attack (parse-attribute attr percent) duration 'type) ...)
+                                            (make-attack (parse-attribute attr2 percent2) duration2 'type2)
+                                            (make-attack (parse-attribute attr3 percent3) duration3 'type3)))])))
 
 (define-syntax define-artifact
   (lambda (stx)
@@ -102,6 +125,32 @@
           #:normal-attacks na:id #:weapon weapon:id #:skill skill:id #:burst burst:id #:artifacts artifact ...)
        #'(define name (make-character hp def atk em critr critd na weapon skill burst (list artifact ...)))])))
 
+(define-syntax define-enemy
+  (lambda (stx)
+    (syntax-parse stx
+      [(_ name #:def def:number
+          #:res (#:pyro pyro:number
+                 #:hydro hydro:number
+                 #:electro electro:number
+                 #:cryo cryo:number
+                 #:geo geo:number
+                 #:anemo anemo:number
+                 #:dendro dendro:number
+                 #:physical physical:number)
+          #:reduction reduction:number)
+       #'(define name (make-enemy def (make-resistances pyro hydro electro cryo geo anemo dendro physical) reduction))])))
+
+(define-syntax define-team-lineup
+  (lambda (stx)
+    (syntax-parse stx
+      [(_ name (chars ...))
+       #'(define name (list chars ...))])))
+
+(define-syntax calculate-rotation-damage
+  (lambda (stx)
+    (syntax-parse stx
+      [(_ lineup enemy (attack-string ...))
+       #'(calc-dmg lineup enemy (list 'attack-string ...))])))
 
 
 
@@ -110,12 +159,12 @@
 ;; wrap this
 
 (define-attack-sequence attack-chain
-  ([(atk 10) 0.5]
-   [(atk 25) 0.2]
-   [(atk 125) 0.8]
-   [(atk 250) 1.5]
-   #:charged [(hp 5) 3.5]
-   #:plunging [(hp 10) 3.5]))
+  ([(atk% 10) 0.5 physical]
+   [(atk% 25) 0.2 physical]
+   [(atk% 125) 0.8 physical]
+   [(atk% 250) 1.5 physical]
+   #:charged [(hp 5) 3.5 pyro]
+   #:plunging [(hp 10) 3.5 physical]))
 
 
 (define-weapon test-weapon
@@ -123,7 +172,7 @@
   (critr 24.1) ;; substat (crit rate)
   (triggered-buff
    [dmgup
-    #:effect (atk 20.0) ;; increase atk by 20%
+    #:effect (atk% 20.0) ;; increase atk by 20%
     #:trigger 'normal-attack
     #:limit 1
     #:party-wide #f
@@ -138,41 +187,42 @@
 ;; note : duration is optional
 (define-skill all-attack-up
   25.0
-  2.0
-  (damage
-   [(atk 125)
-   #:duration 0.1
-   #:type pyro])
+  ; 2.0
+  ; FIX THIS:
+  #:attr (atk% 125)
+  #:duration 0.1
+  #:type pyro
+  #;(damage
+     [(atk 125)
+      #:duration 0.1
+      #:type pyro])
   (applied-buff
    [skill-atkup
-   #:effect (atk 125)
-   #:limit 1
-   #:party-wide #t
-   #:duration 10]) ;; this time applies to all members of a lineup
-)
+    #:effect (atk 125)
+    #:limit 1
+    #:party-wide #t
+    #:duration 10]) ;; this time applies to all members of a lineup
+  )
 
 (define-skill basic-slash
   5.0 ;; cooldown
-  1.0 ;; duration (where character cannot do anything else)
-  (damage
-   [(atk 25)
-   #:duration 0.1
-   #:type pyro]) ;; deal damage equal to 25% of attack, occurs after 0.1 seconds, pyro (fire) damage
-
+  #:attr (atk% 25)
+  #:duration 1.0 ;; duration (where character cannot do anything else)
+  #:type pyro
   (applied-buff
    [skill-hpup
-   #:effect (hp 20)
-   #:limit 1
-   #:party-wide #f
-   #:duration 10.0]) ;; increase hp by 10% of atk, only applies to current character
-)
+    #:effect (hp 20)
+    #:limit 1
+    #:party-wide #f
+    #:duration 10.0]) ;; increase hp by 10% of atk, only applies to current character
+  )
 
 (define-artifact test-feather
   "cool feather collection" ;; set name
   (atk 325) ;; main stat
   (atk 27) ;; substats
   (em 42)
-)
+  )
 
 (define-artifact test-goblet
   "cool goblet collection" ;; set name
@@ -180,7 +230,7 @@
   (critd 16.2) ;; substats
   (critr 3.0)
   (def 128)
-)
+  )
 
 
 (define-character test-char
@@ -195,14 +245,11 @@
   #:skill basic-slash ;; skill
   #:burst all-attack-up ;; burst
   #:artifacts test-feather
-              test-goblet
+  test-goblet
   )
 
-; TODO
-
-#|
 (define-enemy dummy
-  #|#:type Pyro|#
+  #|#:type Pyro|# ; for reactions
   #:def 1000
   #:res (#:pyro 50
          #:hydro 10
@@ -212,10 +259,12 @@
          #:anemo 10
          #:dendro 10
          #:physical -20)
-  #:red 5
+  #:reduction 5
   )
+
+
 
 (define-team-lineup lone-member (test-char))
 
 (calculate-rotation-damage lone-member dummy (N N N C E N ND N ND Q N ND E))
-|#
+
--- runtime.rkt
+++ runtime.rkt
@@ -1,19 +1,38 @@
 #lang racket
 
+(provide make-character
+         make-weapon
+         make-skill
+         make-attack
+         make-artifact
+         make-triggered-buff
+         make-unconditional-buff
+         make-applied-buff
+         make-attack-sequence
+         make-attribute
+         make-percent
+         make-enemy
+         make-resistances
+         calc-dmg)
+
 (define-struct character [hp atk def critr critd em attacks weapon skill burst artifacts])
-(define-struct state-handler [character active-buffs time])
-(define-struct weapon [damage substat buffs])
-(define-struct skill [cd duration buffs])
-(define-struct attack [attr duration])
+(define-struct weapon [atk substat buffs])
+(define-struct skill [cd attr duration type buffs])
+(define-struct attack [attr duration type]) ; split attr
 (define-struct artifact [set-name main-stat substats])
 (define-struct triggered-buff [effect trigger limit party-wide duration])
 (define-struct unconditional-buff [effect party-wide])
 (define-struct applied-buff [effect limit party-wide duration])
 (define-struct attack-sequence [normals charged plunge])
-(define-struct attribute [attr-func value])
-(define-struct damage [attr duration elem-type])
+(define-struct attribute [attr modifier]) ; amount is either number or percent (what is valid depends on attr)
+(define-struct percent [attr p])
+(define-struct enemy [def res reduction])
+(define-struct resistances [pyro hydro electro cryo geo anemo dendro physical])
+; inline this into skill data
+#;(define-struct damage [attr duration elem-type])
 
-(define-struct stat-info [hp def atk em critr critd])
+; internal
+(define-struct stat-info [hp atk def em critr critd])
 (define-struct damage-info [base-dmg base-dmg-mult base-add
                                      dmg-mult
                                      def-mult
@@ -29,14 +48,13 @@
 (define (calc-dmg team enemy attack-string)
   (calc-dmg/acc team enemy attack-string 1 0 0))
 
-(define (calc-dmg/acc team enemy attack-string nc dmg time)
+(define (calc-dmg/acc team enemy attack-string nc dmg time) ; more acc args like buffs (later)
   (cond [(empty? attack-string) (list dmg time)]
         [(cons? attack-string) (cond [(symbol=? 'S (first attack-string)) (calc-dmg/acc (foldr cons (list (first team)) (rest team))
                                                                                         enemy
                                                                                         (rest attack-string)
                                                                                         1
                                                                                         (+ time 1))]
-                                     ; n/c/e/q/nj/nd/cj/cd/ep/jp
                                      [else (let ([nc* (if (symbol=? 'N (first attack-string))
                                                           (if (= nc (length (attack-sequence-normals (character-attacks (first team)))))
                                                               1
@@ -47,47 +65,55 @@
                                                            (rest attack-string)
                                                            nc*
                                                            (+ dmg
-                                                              (calc-single (generate-dmg-info (first team)
-                                                                                              enemy
-                                                                                              (first attack-string)
-                                                                                              nc)))
+                                                              (calc-single-dmg (generate-dmg-info (first team)
+                                                                                                  enemy
+                                                                                                  (first attack-string)
+                                                                                                  nc)))
                                                            (+ time (calc-duration (character-attacks (first team)) (first attack-string) nc))))])]))
 
 (define (calc-duration attacks attack nc)
+  ; n/c/e/q/nj/nd/cj/cd/ep/jp
   (cond [(symbol=? 'N attack) (attack-duration (list-ref (attack-sequence-normals attacks) nc))]
         ))
 
 (define (generate-dmg-info char enemy attack nc)
   (let ([stats (calc-total-stats char)])
     ; un-lambda the attrs (should be runtime job)
-    (cond [(symbol=? 'N attack) (make-damage-info ((attack-attr (list-ref (attack-sequence-normals (character-attacks char)) nc)) stats)
+    ; n/c/e/q/nj/nd/cj/cd/ep/jp
+    (cond [(symbol=? 'N attack) (make-damage-info (calc-attr (attack-attr (list-ref (attack-sequence-normals (character-attacks char)) nc)) stats)
                                                   ; should be in terms of total atk, not base
                                                   1 ; base-dmg-mult
                                                   0 ; base-add
                                                   1 ; dmg-mult
                                                   1 ; def-mult ; TODO
-                                                  1 ; enemy res, use attack-
+                                                  0.5 ; actual enemy res, figure out attack attr
                                                   1 ; amp-mult (reaction)
                                                   (stat-info-critr stats)
                                                   (stat-info-critd stats))]
           )))
 
+(define (calc-attr attr stats)
+  (cond
+    ; must be %?
+    [(symbol=? (attribute-attr attr) 'atk%) (* (attribute-modifier attr) (stat-info-atk stats))]
+    ))
+
 ; do a tree traversal (abstract out, use symbols?) flat incr vs incr by other amt (atk (def% 50)) increases atk by 50% of base def
 (define (calc-total-stats char)
-  (make-stat-info (+ (* char ; base hp
+  (make-stat-info (+ (* (character-hp char) ; base hp
                         (+ 1 #;hp%))
                      #;flat-hp)
-                  (+ (* char ; base def
-                        (+ 1 #;def%))
-                     #;flat-def)
-                  (+ (* (+ char weapon) ; base atk
+                  (+ (* (+ (character-atk char) (weapon-atk (character-weapon char))) ; base atk
                         (+ 1 #;atk%))
                      #;flat-atk)
+                  (+ (* (character-def char) ; base def
+                        (+ 1 #;def%))
+                     #;flat-def)
                   0 #;flat-em-sum
-                  5
-                  50))
+                  5 #;cr-sum
+                  50 #;cd-sum))
 
-(define (calc-single dmg)
+(define (calc-single-dmg dmg)
   (* (+ (* (damage-info-base-dmg dmg) (damage-info-base-dmg-mult dmg)) (damage-info-base-add dmg))
      (damage-info-dmg-mult dmg)
      (damage-info-def-mult dmg)

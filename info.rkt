c5891a29906be8ef485783eb946b286685dfb053
Will Du <duwill1412@gmail.com>
2025-04-18 13:22:04 -0400

    i

--- info.rkt
+++ info.rkt
@@ -3,6 +3,9 @@
 (define name "elements")
 (define deps '("base"
                "rackunit-lib"
-               "syntax-spec-v3"))
+               "syntax-spec-v3"
+               "syntax/parse"
+               "syntax/to-string"
+               "racket/hash"))
 (define license 'MIT)
 (define scribblings '(("scribblings/main.scrbl" () (experimental) "elements")))

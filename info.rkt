5bb0d18d9ff6996e7efa9dd51c87ae62fde804d6
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-04-22 00:12:35 -0400

    #;

--- info.rkt
+++ info.rkt
@@ -4,9 +4,9 @@
 (define deps '("base"
                "rackunit-lib"
                "syntax-spec-v3"
-               "syntax/parse"
-               "scribble/example"
-               "racket/hash"
+               #;"syntax/parse"
+               #;"scribble/example"
+               #;"racket/hash"
                #;"syntax/to-string"))
 (define license 'MIT)
 (define scribblings '(("scribblings/main.scrbl" () (experimental) "elements")))
